3d7135470854687f7f184da77896f4a243815d7a
Oussama Fayz <fzoussfr@gmail.com>
2025-03-27 15:58:53 +0000

    changed init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,28 @@
 require("oussama")
 
+set scrolloff=8
+set number
+set relativenumber
+set tabstop=4 softtabstop=4
+set shiftwidth=4
+set expandtab
+set smartindent
+
+
+
+
+set termguicolors     " enable true colors support
+let ayucolor="dark"
+colorscheme ayu
+
+let mapleader = " "
+nnoremap <leader>pv :Vex<CR>
+nnoremap <leader><CR> :so C:\Users\fayzo\AppData\Local\nvim\init.vim<CR>
+nnoremap <C-p> :GFILES<CR>
+nnoremap <leader>pf :Files<CR>
+nnoremap <C-j>pf :cnext<CR>
+nnoremap <C-k>pf :cprev<CR>
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
